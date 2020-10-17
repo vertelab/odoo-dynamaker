780fe8a50c060a27dbfcee014cc6c2372b92a7cc
Anders Wallenquist <aw@odoo13.vertel.se>
2020-10-17 16:09:52 +0000

    product_dynamic_variants: new module

---
+++ product_dynamic_variants/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': 'Product Dynamic Variant',
+    'version': '1.0',
+    'category': 'Sales',
+    'description': """
+This module adds dynamic variants
+=================================
+
+Adds A variant type to Product Template that:
+--------------------------------------
+    * Can store a float (other data types in future)
+    * Store the data on sale.order.line
+    * Can compute a price due to dynamic changes
+    
+    """,
+    'depends': ['product''],
+    'data': [
+        'views/product_views.xml',
+        
+    ],
+    'website': 'https://www.vertel.se/module/product_dynamic_variant',
+}
