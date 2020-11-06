d381bf7ed22653239060e5f84b22984284cbd694
Lukas Pohlman <lukas.pohlman@vertel.se>
2020-11-06 13:36:35 +0000

    Controller in: 	modified:   product_configurator_dynamaker/models/product.py
    Javascript in:
    	modified:   product_configurator_dynamaker/static/src/js/dynamaker.js
    
    XML in:
    	new file:   product_configurator_dynamaker/views/lukas_view.xml
    
    DONE:
    -- UI is clean and cool.
    -- Price is printed in console window. (i.e. price can be calculated on client side)
    
    TODO:
    -- Problem with controller: URL is not recognized.
    -- Use the return value of the controller to update the price on the webpage.

--- product_configurator_dynamaker/models/product.py
+++ product_configurator_dynamaker/models/product.py
@@ -1,20 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import logging
-#import re
-
 from odoo import api, fields, models, tools, _
-#from odoo.exceptions import ValidationError
-#from odoo.osv import expression
-#from odoo.tools import float_compare
 
-_logger = logging.getLogger(__name__)
-
-class ProductTemplate(models.Model):
-    _inherit = "product.template"
-    dynamaker_url = fields.Char(string='Dynamaker url', size=64, trim=True)
-    
 import json
 import werkzeug
 import itertools
@@ -30,12 +18,37 @@ from odoo.osv import expression
 from odoo.tools import html2plaintext
 from odoo.tools.misc import get_lang
 
+import logging
+_logger = logging.getLogger(__name__)
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+    dynamaker_url = fields.Char(string='Dynamaker url', size=64, trim=True)
+
 class WebsiteProductConfiguratorDynamaker(http.Controller):
     @http.route(['/product_configurator/dynamaker_price'], type='json', auth='public', website=True)
-    def product_configurator_dynamaker_price(self):
-        #domain = request.website.website_domain()
-        #return request.website.viewref('product.product_template_form_view').render({'data': 200, 'value': 201, 'domains': domain, 'open_cfg_step_line_ids': None, 'config_image_vals': None})
-        return request.website.viewref('product.product_template_form_view').render({'price': self.calcProductPrice(0, 0, 0), 'edge-type': None})
+    def product_configurator_dynamaker_price(self, **kw):
+        width = kw.get('width')
+        length = kw.get('length')
+        thickness = kw.get('thickness')
+        edgeType = kw.get('edgeType')
+        
+        _logger.warn("~ width: %s " % width)
+        _logger.warn("~ length: %s " % length)
+        _logger.warn("~ thickness: %s " % thickness)
+        _logger.warn("~ edge: %s " % edgeType)
 
-    def calcProductPrice(self, x, y, z):
-        return x*y*z;
+        price = self.calcProductPrice(width, length, thickness, edgeType)
+        
+        return request.website.viewref('product.product_template_form_view').render({'price': price})
+
+    def calcProductPrice(self, x, y, z, edgeType):
+        edgeTypeCost = 0
+        
+        if edgeType == "polished":
+            edgeTypeCost = 50
+        elif edgeType == "standard":
+            edgeTypeCost = 15
+            
+        # price = 0.5$ per cm³ + edgeType
+        return x*y*z/50 + edgeTypeCost;
--- product_configurator_dynamaker/static/src/js/dynamaker.js
+++ product_configurator_dynamaker/static/src/js/dynamaker.js
@@ -10,18 +10,25 @@ odoo.define("dynamaker_integration_experiment.dynamaker_price_integration", func
         if (event.origin === 'https://deployed.dynamaker.com') {
             try {
                 const parametersFromDynaMaker = event.data    // Structured data coming from DynaMaker
-
+                
+                console.log("parametersFromDynaMaker =", parametersFromDynaMaker)
+                
+                var x = parametersFromDynaMaker["width"]
+                var y = parametersFromDynaMaker["length"]
+                var z = parametersFromDynaMaker["thickness"]
+                
+                var price = self._setProductPrice(x, y, z)
+                console.log("price: ", price)
+                
                 ajax.jsonRpc('/product_configurator/dynamaker_price', 'call', parametersFromDynaMaker).then(
                     function(data) {
                         if (data.error) {
                             console.error(data.error)
                         } else {
-                            var x = data.dimensions['x']
-                            var y = data.dimensions['y']
-                            var z = data.dimensions['z']
-                            var edge_type = data.edge_type
-                            
-                            self._setProductPrice(x, y, z)
+                            // calculated price on serverside
+                            var price = data['price']
+                            console.log("s price: ", price)
+                            // $("#output").html(price);
                         }
                     }
                 )
@@ -33,23 +40,24 @@ odoo.define("dynamaker_integration_experiment.dynamaker_price_integration", func
 });
 
 function _setProductPrice(x, y, z) {
-    return 5;
+    console.log('x = ', x, '; y = ', y, '; z =', z)
+    console.log('x * y * z = ', x*y*z)
+    
+    return x*y*z;
 }
 
 /*
  * replaced
  * <t t-call="website_sale.shop_product_carousel"/>
- * 
  * with
  * <iframe src='https://deployed.dynamaker.com/applications/kvOWnEY7tMC' id='dynamaker-configurator' width="1000" height="700"></iframe>
  * 
  * 
- * installera 'sale_management', 'website_sale (eCommerce)'
- * 
+ * install modules: 'sale_management', 'website_sale (eCommerce)'
  * 
  * "If you edit and save this file, you will never receive updates from Odoo anymore unless you reset your customizations. "
  * 
  * parametersFromDynaMaker = Object { width: 1000, length: 1280, thickness: 10, edgeType: "standard" }
- * * 
+ *
  * */
 
