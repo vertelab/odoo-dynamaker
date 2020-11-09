2c1fc20c815982dd518aa12bf689132ccf5e9092
Lukas Pohlman <lukas.pohlman@vertel.se>
2020-11-09 14:35:52 +0000

    filesystem refactoring

--- product_configurator_dynamaker/static/src/js/dynamaker.js
+++ product_configurator_dynamaker/static/src/js/dynamaker.js
@@ -1,3 +1,6 @@
+/*
+  Catching DynaMaker events and passing them on to Odoo Product Configurator (PLEDRA)
+*/
 odoo.define("dynamaker_integration_experiment.dynamaker_price_integration", function(require) {
     "use strict";
     var ajax = require('web.ajax');
@@ -6,40 +9,25 @@ odoo.define("dynamaker_integration_experiment.dynamaker_price_integration", func
 
     // Define listener for DynaMaker Events
     window.addEventListener('message', (event)=>{
-        if (event.origin === 'https://deployed.dynamaker.com') {
+          // Action for DynaMaker on event
+          if (event.origin === 'https://deployed.dynamaker.com') {
             try {
-                // gets parameters from Dynamaker
-                const parametersFromDynaMaker = event.data    
-                console.log("parametersFromDynaMaker =", parametersFromDynaMaker)
-                
-                // calculate price on client side
-                var x = parametersFromDynaMaker["width"]
-                var y = parametersFromDynaMaker["length"]
-                var z = parametersFromDynaMaker["thickness"]
-                var price = self._setProductPrice(x, y, z)
-                console.log("client price: ", price)
-                
-                // calculate price on serverside
-                ajax.jsonRpc('/product_configurator/dynamaker_price', 'call', parametersFromDynaMaker).then(function(data) {
-                    if (data.error) {
-                        console.error(data.error)
-                    } else {
-                        console.log("server price: ", data['price'])
-                    }
-                })
-           } catch (err) {
-                console.warn('Invalid JSON data from DynaMaker')
-           }
+              const parametersFromDynaMaker = event.data    // Structured data coming from DynaMaker
+              
+              ajax.jsonRpc('/product_configurator/dynamaker_price', 'call', parametersFromDynaMaker).then(function(data) {
+                if (data.error) {
+                  console.error(data.error)
+                } else {
+                   $('span.oe_currency_value').text(data.price)
+                }
+              })
+          } catch (err) {
+            console.warn('Invalid JSON data from DynaMaker')
+          }
+
         }
     })
-});
-
-function _setProductPrice(x, y, z) {
-    console.log('x = ', x, '; y = ', y, '; z =', z)
-    console.log('x * y * z = ', x*y*z)
-    
-    return x*y*z;
-}
+})
 
 /*
  * replaced
@@ -55,41 +43,3 @@ function _setProductPrice(x, y, z) {
  * parametersFromDynaMaker = Object { width: 1000, length: 1280, thickness: 10, edgeType: "standard" }
  *
  * */
-
-/* OLD CODE:
- odoo.define("dynamaker_integration_experiment.dynamaker_price_integration", function(require) {
-    "use strict";
-    var ajax = require('web.ajax');
-
-    console.log("MyTag")
-
-    // Define listener for DynaMaker Events
-    window.addEventListener('message', (event)=>{
-          // Action for DynaMaker on event
-          if (event.origin === 'https://deployed.dynamaker.com') {
-            try {
-              const parametersFromDynaMaker = event.data    // Structured data coming from DynaMaker
-
-              ajax.jsonRpc('/product_configurator/dynamaker_price', 'call', parametersFromDynaMaker).then(function(data) {
-                if (data.error) {
-                  console.error(data.error)
-                } else {
-                  var values = data.value
-                  var domains = data.domain
-                  var open_cfg_step_line_ids = data.open_cfg_step_line_ids
-                  var config_image_vals = data.config_image_vals
-                  self._applyDomainOnValues(domains)
-                  self._setDataOldValId()
-                  self._handleOpenSteps(open_cfg_step_line_ids)
-                  self._setImageUrl(config_image_vals)
-                  self._setWeightPrice(values.weight, values.price, data.decimal_precision)
-                }
-              })
-          } catch (err) {
-            console.warn('Invalid JSON data from DynaMaker')
-          }
-
-        }
-    })
-});
- */
