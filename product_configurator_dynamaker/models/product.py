22e30c81b99d631d5bd15a8db96bc98ec4e46d12
nils-vertel <nils.nyman-waara@vertel.se>
2020-11-13 14:02:54 +0000

    Removed imports that where not used

--- product_configurator_dynamaker/models/product.py
+++ product_configurator_dynamaker/models/product.py
@@ -1,29 +1,11 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import logging
-from odoo import api, fields, models, tools, _
-
-_logger = logging.getLogger(__name__)
+from odoo import api, fields, http, models, tools, _
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
     dynamaker_url = fields.Char(string='Dynamaker url', size=64, trim=True, )
-
-import json
-import werkzeug
-import itertools
-import pytz
-import babel.dates
-from collections import OrderedDict
-
-from odoo import http, fields
-from odoo.addons.http_routing.models.ir_http import slug, unslug
-from odoo.addons.website.controllers.main import QueryURL
-from odoo.http import request
-from odoo.osv import expression
-from odoo.tools import html2plaintext
-from odoo.tools.misc import get_lang
 
 class WebsiteProductConfiguratorDynamaker(http.Controller):
     @http.route(['/product_configurator/dynamaker_price'],
