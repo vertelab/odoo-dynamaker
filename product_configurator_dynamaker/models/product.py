b171fe52a56c40077c05a79c74d3b8bddcd34abc
Lukas Pohlman <lukas.pohlman@vertel.se>
2020-11-20 16:00:27 +0000

    product attribute data is sent to core controller, but in invalid format?

--- product_configurator_dynamaker/models/product.py
+++ product_configurator_dynamaker/models/product.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-
+import json
 from odoo import api, fields, http, models, tools, _
 from odoo.http import request
 from odoo.tools.safe_eval import safe_eval
@@ -11,7 +11,8 @@ _logger = logging.getLogger(__name__)
 class ProductTemplate(models.Model):
     _inherit = "product.template"
     
-    DEFAULT_PYTHON_CODE = """# Specify the python price algorithm here.
+    DEFAULT_PYTHON_CODE = """# Specify the python price algorithm.
+#  - Access the product attributes through the 'kw' dict, eg. kw['width'], and kw['length'].
 #  - Just set the price variable, e.g. price = kw['width'] * kw['length'].
 #\n\n\n\n"""
     
@@ -35,32 +36,23 @@ class WebsiteProductConfiguratorDynamaker(http.Controller):
     @http.route(['/product_configurator/dynamaker_price'], type='json', auth='public', website=True)
     def product_configurator_dynamaker_price(self, **kw):
         # TODO: get id of product through kw. Below is temporary id for Customizable Desk (CONFIG).
-        PRODUCT_ID = 9 
-    
+        PRODUCT_ID = 9
+        
         # get product
         product = request.env['product.template'].browse(PRODUCT_ID)
         
         # calculate price
         price = product._compute_price(**kw)
         
+        if 'form_values' not in request.session:
+            request.session['form_values'] = dict()
+        
+        # store kw in session
+        request.session['form_values'] = json.dumps(kw)
+        
         return {'price': price}
-    
-    # Middle hand before product is added to cart: Adds product attributes to order line before proceeding
-    @http.route(['/product_configurator/add_to_cart'], type='json', auth='public', website=True)
-    def add_product_to_cart(self, **kw):
-        PRODUCT_ID = 9 
-        add_qty = 1
-        set_qty = 0
-        
-        # /usr/share/core-odoo/addons/website_sale/controllers/main.py:420
-        request.env['sale.order'].cart_update(PRODUCT_ID, add_qty, set_qty, kw)
-        
-        _logger.warn("~ DONE! orderrads extra attribut = %s" % sale_order_line.product_custom_attribute_value_ids)
 
-# create a sale.order.line if not already exist (TODO: else update)
-#sale_order_line = request.env['sale.order.line'].create({'order_id': ORDER_ID, 'name': product.name, 'price_unit': price, 'product_uom_qty': 1, 'customer_lead' : 99999})
-
-# create that sale_order_line's product_custom_attribute_value_ids
-#for attribute, value in kw.items():
-#    sale_order_line.product_custom_attribute_value_ids.write((0, _, {'sale_order_line_id': sale_order_line.id, 'name': attribute, 'custom_value': value}))
+        
+        
+        
         
