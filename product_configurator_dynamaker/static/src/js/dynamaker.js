08861232e47113041972b46605968d87dbade498
victorekstrom <victor.ekstrom@vertel.se>
2021-03-08 09:47:53 +0000

    indentering fix igen

--- product_configurator_dynamaker/static/src/js/dynamaker.js
+++ product_configurator_dynamaker/static/src/js/dynamaker.js
@@ -22,7 +22,7 @@ odoo.define("dynamaker_integration_experiment.dynamaker_price_integration", func
                         console.error(data.error)
                     } else {
                         $('span.oe_currency_value').text(data.price)
-					    $(document.getElementsByClassName("product_price mt16"))[0].setAttribute('class','product_price mt16')
+                        $(document.getElementsByClassName("product_price mt16"))[0].setAttribute('class','product_price mt16')
                     }
                 })
             } catch (err) {
