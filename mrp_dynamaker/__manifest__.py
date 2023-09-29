93b61fc4e485d1544fd4cafe3946c730adfdd9c2
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-09-29 11:07:18 +0200

    Update __manifest__.py

--- mrp_dynamaker/__manifest__.py
+++ mrp_dynamaker/__manifest__.py
@@ -1,15 +1,43 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2023- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 {
     'name': 'Dynamaker MRP',
     'version': '1.2',
-    'category': 'MRP',
-    'depends': ['product_configurator_dynamaker', 'mrp'],
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Dynamaker extension to the mrp module.',
+    'category': 'Factory',
     'description': """
-Dynamaker extension to the mrp module
-
+    Dynamaker extension to the mrp module.
     """,
+    #'sequence': '1'
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-dynamaker/mrp_dynamaker',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-dynamaker',
+
+    'depends': ['product_configurator_dynamaker', 'mrp'],
     'data': [
      'views/product_views.xml',
     ],
